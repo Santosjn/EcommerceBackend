ecd2aa2379407187526cb2efb93f8be12631a8aa
Jean Novaes Santos <jeansjcsp@gmail.com>
2017-07-19 19:18:20 -0300

    Adding configuration to try to prevent CORS related problems.

--- Ecommerce/src/main/java/com/ecommerce/controller/ProductController.java
+++ Ecommerce/src/main/java/com/ecommerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecommerce.model.Product;
 import com.ecommerce.service.ProductService;
 
+@CrossOrigin
 @RestController
 public class ProductController {
 
